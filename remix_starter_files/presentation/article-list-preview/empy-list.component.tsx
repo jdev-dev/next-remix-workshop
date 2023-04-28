bb66d3afb5c87cdb41dfd68e03eeedd86ed314d5
Eligio Becerra Zavala <ebz@grupoesoft.com>
2023-04-28 14:10:45 +0000

    Updated remix files

---
+++ remix_starter_files/presentation/article-list-preview/empy-list.component.tsx
@@ -0,0 +1,7 @@
+import type { ReactElement } from "react";
+
+export default function EmptyList(): ReactElement {
+    return <div className="article-preview">
+        <p>Sorry there are no articles available</p>
+    </div>
+}
